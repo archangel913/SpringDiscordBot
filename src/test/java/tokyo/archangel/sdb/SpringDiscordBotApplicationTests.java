aec5464bace839ec31191bd45578c238837974ca
archangel913 <archangel.yt.0913@gmail.com>
2026-01-04 18:46:49 +0900

    階層の修正

---
+++ src/test/java/tokyo/archangel/sdb/SpringDiscordBotApplicationTests.java
@@ -0,0 +1,13 @@
+package tokyo.archangel.sdb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDiscordBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
